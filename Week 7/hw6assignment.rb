bf73172924b370cbcae203d603b70a70278c4a49
ljsong <logensong@gmail.com>
2022-11-01 17:53:11 +0800

    finish homework for week 8

--- Week 7/hw6assignment.rb
+++ Week 7/hw6assignment.rb
@@ -29,7 +29,7 @@ class MyBoard < Board
   def initialize(game)
     @grid = Array.new(num_rows) {Array.new(num_columns)}
     @current_block = MyPiece.next_piece(self)
-    @score = 1000
+    @score = 0
     @game = game
     @delay = 500
   end
